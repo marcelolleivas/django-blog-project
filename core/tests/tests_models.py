773e74aba7a09f841a058cd363dfb31e42f178dc
Marcelo Leivas <marceloleivas@Marcelos-MacBook-Pro.local>
2021-05-29 10:08:38 -0300

    post model testing finalized + coverage settings refactored

--- core/tests/tests_models.py
+++ core/tests/tests_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .conftest import PostFactory
 
@@ -6,6 +7,24 @@ from .conftest import PostFactory
 class TestModelPost(TestCase):
     def setUp(self) -> None:
         self.post = PostFactory()
+
+    def test_absolute_url_delete(self) -> None:
+        # Given
+        url = self.post.get_absolute_url_delete()
+        # Then
+        self.assertEqual(url, "/post/lorem-ipsum-dolor/delete/")
+
+    def test_absolute_url_update(self) -> None:
+        # Given
+        url = self.post.get_absolute_url_update()
+        # Then
+        self.assertEqual(url, "/post/lorem-ipsum-dolor/edit/")
+
+    def test_absolute_url(self) -> None:
+        # Given
+        url = self.post.get_absolute_url()
+        # Then
+        self.assertEqual(url, "/post/lorem-ipsum-dolor/")
 
     def test_str_method_returns_title(self) -> None:
         # Given
